0bff862dc827d92e77a5da476c95cb52d06d5aa4
NightFish <NightFish.net@gmail.com>
2023-03-26 00:43:52 +1300

    Run code cleanup

--- src/YikesBot/Services/DeletedMessages/DeletedMessagesLogger.cs
+++ src/YikesBot/Services/DeletedMessages/DeletedMessagesLogger.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using Discord;
+﻿using Discord;
 using Discord.Rest;
 using Discord.WebSocket;
 using Microsoft.Extensions.Logging;
@@ -17,45 +16,49 @@ public class DeletedMessagesLogger
     {
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         _ = discordBot ?? throw new ArgumentNullException(nameof(discordBot));
-        
+
         discordBot.DiscordClient.MessageDeleted += DiscordClientOnMessageDeleted;
         discordBot.DiscordClient.GuildAvailable += CreateLogChannelAsync;
     }
 
-    private async Task DiscordClientOnMessageDeleted(Cacheable<IMessage, ulong> cachableMessage, Cacheable<IMessageChannel, ulong> cachableChannel)
+    private async Task DiscordClientOnMessageDeleted(Cacheable<IMessage, ulong> cachableMessage,
+        Cacheable<IMessageChannel, ulong> cachableChannel)
     {
         if (await cachableChannel.GetOrDownloadAsync() is SocketGuildChannel channel)
         {
-            IMessageChannel logChannel = await GetLogChannelAsync(channel.Guild);
-            IMessage message = cachableMessage.Value;
+            var logChannel = await GetLogChannelAsync(channel.Guild);
+            var message = cachableMessage.Value;
             if (message == null) return; // If the message is not in our cache we cannot get the content
 
             IUser? suspectedDeleter = null;
             var auditLogs = channel.Guild.GetAuditLogsAsync(10, actionType: ActionType.MessageDeleted);
-            await foreach(var logs in auditLogs)
+            await foreach (var logs in auditLogs)
             {
                 if (suspectedDeleter != null) break;
                 foreach (var log in logs)
                 {
                     if (!(log.Data is MessageDeleteAuditLogData messageDeleteData)) continue;
                     if (messageDeleteData.ChannelId != channel.Id) continue;
-                    if (messageDeleteData.Target.Id  != message.Author.Id) continue;
-                    if (log.CreatedAt < DateTimeOffset.UtcNow - TimeSpan.FromMinutes(10)) continue; // Multiple message deletes get saved under the first timestamp
+                    if (messageDeleteData.Target.Id != message.Author.Id) continue;
+                    if (log.CreatedAt < DateTimeOffset.UtcNow - TimeSpan.FromMinutes(10))
+                        continue; // Multiple message deletes get saved under the first timestamp
 
                     suspectedDeleter = log.User;
                     break;
                 }
             }
-            
+
             if (!string.IsNullOrWhiteSpace(message.Content))
             {
-                _logger.LogInformation("Logging deleted message from {Author} in {Guild}/{Channel}", 
+                _logger.LogInformation("Logging deleted message from {Author} in {Guild}/{Channel}",
                     message.Author.Username + "#" + message.Author.Discriminator,
                     channel.Guild.Name,
                     channel.Name);
                 var embed = new EmbedBuilder()
                     .WithDescription($"**Message from {message.Author.Mention} deleted in <#{message.Channel.Id}> " +
-                                     (suspectedDeleter != null ? $" probably by {suspectedDeleter.Mention}" : string.Empty) + 
+                                     (suspectedDeleter != null
+                                         ? $" probably by {suspectedDeleter.Mention}"
+                                         : string.Empty) +
                                      "**\n" + message.Content)
                     .WithColor(new Color(254, 204, 80))
                     .WithCurrentTimestamp()
@@ -67,23 +70,25 @@ public class DeletedMessagesLogger
                     .WithFooter($"User: {message.Author.Id} • Message: {message.Id}");
                 if (suspectedDeleter != null)
                 {
-                    
                 }
-                await logChannel.SendMessageAsync(String.Empty,  embed: embed.Build());
+
+                await logChannel.SendMessageAsync(string.Empty, embed: embed.Build());
             }
 
             foreach (var attachment in message.Attachments)
             {
                 if (!attachment.ContentType.StartsWith("image")) continue;
-                
-                _logger.LogInformation("Logging deleted image from {Author} in {Guild}/{Channel}", 
+
+                _logger.LogInformation("Logging deleted image from {Author} in {Guild}/{Channel}",
                     message.Author.Username + "#" + message.Author.Discriminator,
                     channel.Guild.Name,
                     channel.Name);
-                
+
                 var embed = new EmbedBuilder()
                     .WithDescription($"**Image from {message.Author.Mention} deleted in <#{message.Channel.Id}>" +
-                                     (suspectedDeleter != null ? $" probably by {suspectedDeleter.Mention}" : string.Empty) + 
+                                     (suspectedDeleter != null
+                                         ? $" probably by {suspectedDeleter.Mention}"
+                                         : string.Empty) +
                                      "**\n" + message.Content)
                     .WithColor(new Color(254, 204, 80))
                     .WithCurrentTimestamp()
@@ -94,24 +99,25 @@ public class DeletedMessagesLogger
                         x.IconUrl = message.Author.GetAvatarUrl(ImageFormat.Auto, 256);
                     })
                     .WithFooter($"User: {message.Author.Id} • Message: {message.Id}");
-                await logChannel.SendMessageAsync(String.Empty,  embed: embed.Build());
+                await logChannel.SendMessageAsync(string.Empty, embed: embed.Build());
             }
         }
     }
 
     private async Task<IMessageChannel> GetLogChannelAsync(IGuild guild)
     {
-        var channels = await guild.GetTextChannelsAsync(CacheMode.AllowDownload);
+        var channels = await guild.GetTextChannelsAsync();
         return channels.First(x => x.Name.Equals(LogChannelName));
     }
 
     private async Task CreateLogChannelAsync(IGuild guild)
     {
-        var channels = await guild.GetTextChannelsAsync(CacheMode.AllowDownload);
+        var channels = await guild.GetTextChannelsAsync();
         if (!channels.Any(x => x.Name.Equals(LogChannelName)))
         {
-            _logger.LogInformation("Creating deleted messages channel in guild {GuildName} {GuildID}", guild.Name, guild.Id);
-            var permissionOverrides = new OverwritePermissions(viewChannel:PermValue.Deny);
+            _logger.LogInformation("Creating deleted messages channel in guild {GuildName} {GuildID}", guild.Name,
+                guild.Id);
+            var permissionOverrides = new OverwritePermissions(viewChannel: PermValue.Deny);
             var logChannel = await guild.CreateTextChannelAsync(LogChannelName);
             await logChannel.AddPermissionOverwriteAsync(guild.EveryoneRole, permissionOverrides);
         }
