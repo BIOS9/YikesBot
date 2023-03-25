03e0cc3c0bab9b5236449744c1b449155cdc875a
NightFish <NightFish.net@gmail.com>
2023-03-25 19:23:58 +1300

    Add user secrets to config

--- src/YikesBot/Program.cs
+++ src/YikesBot/Program.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 using YikesBot.Services.DiscordBot;
 
 await Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(config =>
+    {
+        config.AddEnvironmentVariables();
+        config.AddUserSecrets<Program>();
+    })
     .UseSerilog((context, config) =>
     {
         config.ReadFrom.Configuration(context.Configuration);
