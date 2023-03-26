056ea27c51d3a957039fe47f0077f5f9249257d3
NightFish <NightFish.net@gmail.com>
2023-03-26 22:05:18 +1300

    Add awoo message content handler

---
+++ src/YikesBot/Services/MessageContent/IContentHandler.cs
@@ -0,0 +1,10 @@
+﻿using Discord.WebSocket;
+
+namespace YikesBot.Services.MessageContent;
+
+public interface IContentHandler
+{
+    string Name { get; }
+    Task<bool> IsMatchAsync(SocketMessage message);
+    Task<bool> ExecuteAsync(SocketMessage message);
+}
