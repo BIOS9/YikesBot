9209471793b7c309a78ec69e068d37ce542f6c89
NightFish <NightFish.net@gmail.com>
2023-03-26 23:19:40 +1300

    Add furry speaker service

---
+++ src/YikesBot/Services/Furry/ServicesConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace YikesBot.Services.Furry;
+
+public static class ServicesConfiguration
+{
+    public static IServiceCollection AddFurrySpeaker(this IServiceCollection services)
+    {
+        services.AddSingleton<FurrySpeaker>();
+        return services;
+    }
+}
