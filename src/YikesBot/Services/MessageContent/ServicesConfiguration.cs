27d1e37514f6e0de7bbc131b643509318d1ecd77
NightFish <NightFish.net@gmail.com>
2023-03-26 21:47:14 +1300

    Add message content handler service

---
+++ src/YikesBot/Services/MessageContent/ServicesConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace YikesBot.Services.MessageContent;
+
+public static class ServicesConfiguration
+{
+    public static IServiceCollection AddMessageContentHandler(this IServiceCollection services)
+    {
+        services.AddSingleton<MessageContentHandler>();
+        return services;
+    }
+}
