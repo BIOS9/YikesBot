d9d551c627f74acd936da1875205c9cc809114ac
NightFish <NightFish.net@gmail.com>
2023-04-13 13:24:40 +1200

    Convert deleted messages to autofac

---
+++ src/YikesBot/Services/DeletedMessages/DeletedMessagesModule.cs
@@ -0,0 +1,11 @@
+﻿using Autofac;
+
+namespace YikesBot.Services.DeletedMessages;
+
+public class DeletedMessagesModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<DeletedMessagesLogger>().SingleInstance();
+    }
+}
