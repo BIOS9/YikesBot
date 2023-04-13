2bbbfe488e66dd6d33f9cffe28013010b22cac74
NightFish <NightFish.net@gmail.com>
2023-04-13 13:27:35 +1200

    Convert message content to autofac

---
+++ src/YikesBot/Services/MessageContent/MessageContentModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using YikesBot.Services.MessageContent.ContentHandlers;
+
+namespace YikesBot.Services.MessageContent;
+
+public class MessageContentModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<MessageContentHandler>().SingleInstance();
+        builder.RegisterType<AwooHandler>().As<IContentHandler>();
+    }
+}
