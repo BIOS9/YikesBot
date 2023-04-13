5303c18e45911910fd99d1636224cbb464bf4488
NightFish <NightFish.net@gmail.com>
2023-04-13 13:31:05 +1200

    Convert slash commands to autofac

---
+++ src/YikesBot/Services/SlashCommands/SlashCommandsModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using YikesBot.Services.SlashCommands.Commands;
+
+namespace YikesBot.Services.SlashCommands;
+
+public class SlashCommandsModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<SlashCommandHandler>().SingleInstance();
+        builder.RegisterType<PurgeCommand>().As<ICommand>();
+        builder.RegisterType<MicropadCommand>().As<ICommand>();
+        builder.RegisterType<FurryspeakCommand>().As<ICommand>();
+    }
+}
