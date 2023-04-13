09b061537ef979be8b125a58e3d273fb44c2baca
NightFish <NightFish.net@gmail.com>
2023-04-13 13:28:58 +1200

    Convert message reactions to autofac

---
+++ src/YikesBot/Services/MessageReactions/MessageReactions.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using YikesBot.Services.MessageReactions.ReactionHandlers;
+
+namespace YikesBot.Services.MessageReactions;
+
+public class MessageReactions : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<MessageReactionHandler>().SingleInstance();
+        builder.RegisterType<PinReactHandler>().As<IReactionHandler>();
+    }
+}
