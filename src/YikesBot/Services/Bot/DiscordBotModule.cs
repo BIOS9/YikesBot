069d66589160a95e4ebb22f2cf5e361b5b82dd76
NightFish <NightFish.net@gmail.com>
2023-04-13 13:22:28 +1200

    Convert discord bot to autofac

---
+++ src/YikesBot/Services/Bot/DiscordBotModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using YikesBot.Helpers;
+
+namespace YikesBot.Services.Bot;
+
+public class DiscordBotModule : Module
+{
+    private readonly IConfiguration _configuration;
+
+    public DiscordBotModule(IConfiguration configuration)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<DiscordBot>().AsSelf().As<IHostedService>().SingleInstance().PropertiesAutowired();
+        builder.ConfigureWithValidation<DiscordBotOptions>(
+            _configuration.GetExistingSectionOrThrow(DiscordBotOptions.Name));
+    }
+}
