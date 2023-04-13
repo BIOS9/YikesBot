8c63a27ca22976aaab274933f7ffa925b4b5031e
NightFish <NightFish.net@gmail.com>
2023-04-13 13:26:13 +1200

    Convert furry to autofac

---
+++ src/YikesBot/Services/Furry/FurryModule.cs
@@ -0,0 +1,11 @@
+﻿using Autofac;
+
+namespace YikesBot.Services.Furry;
+
+public class FurryModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<FurrySpeaker>().SingleInstance();
+    }
+}
